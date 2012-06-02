fd9009910b790ef7eb905b28431a9c46e5da7a31
Nick Sewell <nsewell@gmail.com>
2012-06-02 19:30:25 +1000

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120602025902) do
+ActiveRecord::Schema.define(:version => 20120602080150) do
 
   create_table "bills", :force => true do |t|
     t.string   "title"
@@ -154,6 +153,20 @@ ActiveRecord::Schema.define(:version => 20120602025902) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "speeches", :force => true do |t|
+    t.date     "date"
+    t.integer  "parliament_number"
+    t.integer  "session_number"
+    t.integer  "period_number"
+    t.string   "member_id"
+    t.string   "electorate"
+    t.string   "party"
+    t.string   "role"
+    t.text     "content"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
   end
 
   create_table "titles", :force => true do |t|
