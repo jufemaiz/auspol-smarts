d6070732febdd31ae3582ae80768c84fd7f8d548
Joel Courtney <euphemize@gmail.com>
2012-06-07 16:18:39 +1000

    Downcase for statistical analysis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     unless aphkey.nil?
       m = Member.find_by_aphkey(aphkey)
       kill = Set.new
-      ["Australia", "Australian","member","Minister","motion"].each{|w| kill << w}
+      ["Australia", "Australian","member","Minister","motion"].each{|w| kill << w.downcase}
       kill << m.first_name
       kill << m.last_name
       kill << m.constituency
@@ -12,7 +12,7 @@ module ApplicationHelper
       key = m.aphkey
       words = Hash.new(0)
       Speech.where(:member_id => m.aphkey).each do |speech|
-        speech.content.split(/[\W]+/).delete_if{|w| ((w.length <= 4) || (kill.include?(w))) }.each{|w| words[w]+=1 }
+        speech.content.split(/[\W]+/).delete_if{|w| ((w.length <= 4) || (kill.include?(w.downcase))) }.each{|w| words[w.downcase]+=1 }
       end
       return words.sort_by{|x,y| y}.last(50).reverse
     end
